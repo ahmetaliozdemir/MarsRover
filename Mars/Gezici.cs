51b7b4943784b45784f6f1e15353a2840d25c86b
Ahmet Ali Özdemir <ahmetali131011@gmail.com>
2021-11-05 12:00:33 +0300

    Gezicinin mevcut dönmüş olduğu yöne hareket etmesini sağlayan fonksiyon eklendi.

--- Mars/Gezici.cs
+++ Mars/Gezici.cs
@@ -32,6 +32,27 @@ namespace Mars
 
         }
 
+        public string Move(int xlimit, int ylimit)
+        {
+            switch (yon_sayisi)
+            {
+                case 0:
+                    y = y + 1 > ylimit ? ylimit : y + 1;
+                    break;
+                case 1:
+                    x = x + 1 > xlimit ? xlimit : x + 1;
+                    break;
+                case 2:
+                    y = y - 1 < 0 ? 0 : y - 1;
+                    break;
+                case 3:
+                    x = x - 1 < 0 ? 0 : x - 1;
+                    break;
+            }
+
+            return bilgi();
+        }
+
         public string bilgi()
         {
             return (x.ToString() + "," + y.ToString() + "," + Yonlistesi.Find(y => y.Id == yon_sayisi).yon);
