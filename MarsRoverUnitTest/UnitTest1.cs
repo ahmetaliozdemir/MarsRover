97ab034cd4ba2c4a5eb735a594cdd31157951c30
Ahmet Ali Özdemir <ahmetali131011@gmail.com>
2021-11-05 12:41:09 +0300

    Gezicilerin hareketleri girilerek testler yazıldı ve doğru sonuçlar alındı.

--- MarsRoverUnitTest/UnitTest1.cs
+++ MarsRoverUnitTest/UnitTest1.cs
@@ -1,3 +1,4 @@
+using Mars;
 using System;
 using Xunit;
 
@@ -5,10 +6,44 @@ namespace MarsRoverUnitTest
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        Plato plato = new Plato(5, 5);
 
+        [Fact]
+        public void Gezici1()
+        {
+            plato.GeziciEkle(1, 2, 'N');
+
+            plato.GeziciHareket(0, 'l');
+            plato.GeziciHareket(0, 'm');
+            plato.GeziciHareket(0, 'l');
+            plato.GeziciHareket(0, 'm');
+            plato.GeziciHareket(0, 'l');
+            plato.GeziciHareket(0, 'm');
+            plato.GeziciHareket(0, 'l');
+            plato.GeziciHareket(0, 'm');
+            string durum = plato.GeziciHareket(0, 'm');
+
+            Assert.Equal("1,3,N", durum);
+        }
+        
+        [Fact]
+        public void Gezici2()
+        {
+            plato.GeziciEkle(3, 3, 'E');
+
+            plato.GeziciHareket(0, 'm');
+            plato.GeziciHareket(0, 'm');
+            plato.GeziciHareket(0, 'r');
+            plato.GeziciHareket(0, 'm');
+            plato.GeziciHareket(0, 'm');
+            plato.GeziciHareket(0, 'r');
+            plato.GeziciHareket(0, 'm');
+            plato.GeziciHareket(0, 'r');
+            plato.GeziciHareket(0, 'r');
+
+            string durum = plato.GeziciHareket(0, 'm');
+
+            Assert.Equal("5,1,E", durum);
         }
     }
 }
