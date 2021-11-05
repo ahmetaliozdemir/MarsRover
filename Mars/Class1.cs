e8cc30c6ba77278d551152f96cd83457bbc4d964
Ahmet Ali Özdemir <ahmetali131011@gmail.com>
2021-11-05 11:38:24 +0300

    Marsta bulunan plato ile ilgili işlemlerimizi yapacak olan sınıfımız oluşturuldu ve Sınıf oluşturulurken platonun sınırları belirlenecek şekilde düzenlendi.

--- Mars/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Mars
-{
-    public class Class1
-    {
-    }
-}
---
+++ Mars/Plato.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Mars
+{
+    public class Plato
+    {
+        //Platonun sınırlarını belli eden x ve y değerleri.
+        private int x;
+        private int y;
+
+        public Plato(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+    }
+}
