9c313922eba411bd4091c1b2f21404df3dfea918
Ahmet Ali Özdemir <ahmetali131011@gmail.com>
2021-11-05 11:31:48 +0300

    Add project files.

---
+++ MarsRover/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarsRover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
