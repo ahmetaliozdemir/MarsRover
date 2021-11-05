00355994c7dfedce61aa99426264b0ce6cd1394f
Ahmet Ali Özdemir <ahmetali131011@gmail.com>
2021-11-05 12:19:06 +0300

    Plato içerisinde yer alacak geziciler eklnecek.

--- MarsRover/Program.cs
+++ MarsRover/Program.cs
@@ -13,6 +13,20 @@ namespace MarsRover
             int ylimit = Convert.ToInt32(Console.ReadLine());
             Plato plato = new Plato(xlimit, ylimit);
 
+
+            Console.WriteLine("Kaç tane gezici var ?");
+            int geziciSayisi = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < geziciSayisi; i++)
+            {
+                Console.WriteLine("X konumunu giriniz :");
+                int x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Y konumunu giriniz :");
+                int y = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Yönünü giriniz (N,E,W,S) :");
+                char yon = Convert.ToChar(Convert.ToString(Console.ReadLine()).ToUpper());
+                plato.GeziciEkle(x, y, yon);
+            }
+
             Console.ReadLine();
 
         }
