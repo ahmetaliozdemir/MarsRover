2da8391f0caa6c43143e7532f0e8db34a6661222
Ahmet Ali Özdemir <ahmetali131011@gmail.com>
2021-11-05 11:54:25 +0300

    Gezicinin yer aldığı konumu ve yonünü tespit etmek için gerekli değerler tanımlandı. Platoda tanımlanmış yönler alındı ve yonu belirlemiz için yon sayımız ile eşleştirildi.

--- Mars/Gezici.cs
+++ Mars/Gezici.cs
@@ -6,6 +6,17 @@ namespace Mars
 {
     class Gezici
     {
-
+        private int x;
+        private int y;
+        private int yon_sayisi;
+        public List<Yon> Yonlistesi;
+        public Gezici(int _x, int _y, char _yon, List<Yon> Yonler)
+        {
+            x = _x;
+            y = _y;
+            Yonlistesi = Yonler;
+            //Yön sayısı ve yon id eşleştirilerek yon sayısı tespit edildi.
+            yon_sayisi = Yonlistesi.Find(y => y.yon == _yon).Id;
+        }
     }
 }
