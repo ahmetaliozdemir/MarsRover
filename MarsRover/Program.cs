42de109e9212cd1198ab8ad9d1de0b752742650e
Ahmet Ali Özdemir <ahmetali131011@gmail.com>
2021-11-05 12:29:13 +0300

    Gezicilerin birinin işlemleri bitmeden diğerine geçmeyecek şekilde işlem bitene kadar girdiğiniz işleme göre işlem yapması eklendi.

--- MarsRover/Program.cs
+++ MarsRover/Program.cs
@@ -27,6 +27,41 @@ namespace MarsRover
                 plato.GeziciEkle(x, y, yon);
             }
 
+            int gezicisayisi = plato.GeziciSayisi();
+
+            for (int i = 0; i < gezicisayisi; i++)
+            {
+                
+                bool durum = true;
+                string gezicikonum = "";
+
+                while (durum)
+                {
+                    Console.WriteLine("Gezici hareketlerine devam etmek istiyormusunuz ? (y/n)");
+                    string devam = Console.ReadLine();
+                    bool cevap = devam == "y" ? true : false;
+                    if (cevap)
+                    {
+                        Console.WriteLine("İşlemler Right(R),Left(L),Move(M) birini seçebilirsiniz.");
+                        char islem = Convert.ToChar(Console.ReadLine());
+                        gezicikonum = plato.GeziciHareket(i, islem);
+                        Console.WriteLine(gezicikonum);
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Gezicinin mevcut konumu " + gezicikonum);
+                        Console.WriteLine("Bir gezicinin işlemleri tamamlandı.");
+                        durum = false;
+                        break;
+                    }
+                }
+                
+
+            }
+
+            Console.WriteLine("Başka bir gezici kalmadı işlem bitti.");
+
             Console.ReadLine();
 
         }
